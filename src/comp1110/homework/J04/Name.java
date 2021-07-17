b1a2bf6ee0a4f6aa666832fb8fb1fbf75340ee64
Dennis <7401444281@qq.com>
2021-07-17 18:44:34 +0800

    modifiy lec2_homework

--- src/comp1110/homework/J04/Name.java
+++ src/comp1110/homework/J04/Name.java
@@ -2,11 +2,5 @@ package comp1110.homework.J04;
 import java.util.Scanner;
 
 public class Name {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String Name= in.nextLine();
-        int a = Name.length();
-        System.out.println(Name.toUpperCase());
-        System.out.println(a);
-    }
+
 }
--- src/comp1110/homework/J04/Radix.java
+++ src/comp1110/homework/J04/Radix.java
@@ -2,14 +2,5 @@ package comp1110.homework.J04;
 import java.util.Scanner;
 
 public class Radix {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int base =in.nextInt();
-        int dest = in.nextInt();
-        String c = in.next();
-        int mid = Integer.parseInt(c,base);
-        String out = Integer.toString(mid,dest);
-        System.out.println(out);
 
-    }
 }
